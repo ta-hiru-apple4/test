9ee3de1a519c6b161cde5bb825d29372f5ba74e1
kagasan <harukik0504@gmail.com>
2022-03-02 15:08:19 +0900

    変えました

--- b.py
+++ b.py
@@ -1,5 +1,14 @@
-import datetime 
+import datetime
+from distutils.command.build_scripts import first_line_re
+import imp 
 import time
+import requests
+from bs4 import BeautifulSoup 
 b=datetime.datetime.fromtimestamp(time.time())
 print("時刻")
 print(b)
+r=requests.get("http://www.cis1.c.dendai.ac.jp/")
+print(r.headers)
+print(r.encoding)
+a=BeautifulSoup(r.content,"html.parser")
+print(a.find())
